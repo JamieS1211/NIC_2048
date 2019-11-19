1b3f22cb8c627d19e2602347ed568b5c3931ccb9
JamieS1211 <jamieshepherd1211@gmail.com>
2019-11-19 15:30:13 +0000

    Initial - Run agentEvaluation

---
+++ src/main/java/nic/AgentEvaluation.java
@@ -0,0 +1,32 @@
+package nic;
+
+import org.apache.commons.math3.random.MersenneTwister;
+import org.apache.commons.math3.random.RandomDataGenerator;
+import put.game2048.Agent;
+import put.game2048.Game;
+import put.game2048.MultipleGamesResult;
+
+import java.time.Duration;
+import java.util.function.Supplier;
+
+public class AgentEvaluation {
+	public static void main(String[] args) {
+		int action_time_limit_ms = 1000;
+		int num_games = 10;
+		String random_seed = "1234";
+
+			final Supplier<Agent> AGENT = createAgentFactoryByReflection();
+			final int REPEATS = num_games;
+			final Duration ACTION_TIME_LIMIT = Duration.ofNanos(action_time_limit_ms * 1000 * 1000);
+			final long RANDOM_SEED = Long.parseLong(random_seed);
+
+			RandomDataGenerator random = new RandomDataGenerator(new MersenneTwister(RANDOM_SEED));
+			MultipleGamesResult result = new Game(ACTION_TIME_LIMIT).playMultiple(AGENT, REPEATS, random);
+			System.out.println(result.toCvsRow());
+
+	}
+
+	private static Supplier<Agent> createAgentFactoryByReflection() {
+		return () -> new CustomAgent();
+	}
+}
---
+++ src/main/java/nic/CustomAgent.java
@@ -0,0 +1,22 @@
+package nic;
+
+import java.time.Duration;
+import java.util.List;
+
+import com.google.common.base.Preconditions;
+import org.apache.commons.math3.random.MersenneTwister;
+import org.apache.commons.math3.random.RandomDataGenerator;
+import put.ci.cevo.util.RandomUtils;
+import put.game2048.Action;
+import put.game2048.Agent;
+import put.game2048.Board;
+
+public class CustomAgent implements Agent {
+	public RandomDataGenerator random = new RandomDataGenerator(new MersenneTwister(123));
+
+	public Action chooseAction(Board board, List<Action> possibleActions, Duration maxTime) {
+		Preconditions.checkArgument(0 < possibleActions.size());
+
+		return (Action)RandomUtils.pickRandom(possibleActions.toArray(), random);
+	}
+}
