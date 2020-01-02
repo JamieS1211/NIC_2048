12ead33136d904d8241b6fcf4a23dfb343d8c609
phoenixwilliams16 <phoenixwilliams16@googlemail.com>
2020-01-02 19:37:44 +0000

    Multi Layered NN complete, has good prediction accuracy with simple tasks, next step is to use game data

--- src/main/java/nic/MultLayeredNN.java
+++ src/main/java/nic/MultLayeredNN.java
@@ -1,5 +1,8 @@
 package nic;
 
+
+//import src.Matrix;
+
 public class MultLayeredNN {
 	
 	float input_nodes;
@@ -12,7 +15,7 @@ public class MultLayeredNN {
 	
 	
 	
-	MultLayeredNN(float[] layers){
+	MultLayeredNN(float[] layers, float lr){
 		//layers will define the shape of the NN
 		this.input_nodes = layers[0];
 		this.output_nodes = layers[layers.length-1];
@@ -34,7 +37,7 @@ public class MultLayeredNN {
 			this.biases[i].randomize();
 		}
 		
-		this.learning_rate = (float) 0.1;
+		this.learning_rate = lr;
 		
 	}
 	
@@ -61,7 +64,7 @@ public class MultLayeredNN {
 			if (m == 0) {
 				current_gen = Matrix.multiply(this.weights[m], inputs);
 				current_gen = Matrix.add(current_gen, this.biases[m]);
-				//current_gen.print();
+				
 				
 				for (int i=0; i<current_gen.rows;i++) {
 					for (int j=0; j<current_gen.cols;j++) {
@@ -80,8 +83,138 @@ public class MultLayeredNN {
 			
 			}
 		}
+		
 		return current_gen.toArray();
 	}
+	
+	
+void train(float[] input_array, float[] target_array) {
+	
+		//System.out.println("training....");
+
+		
+		///// FEEDFORWARD START //////
+		Matrix inputs = Matrix.fromArray(input_array);
+		Matrix current_gen = null;
+		Matrix[] hiddens = new Matrix[this.weights.length];
+		//hiddens[0] = inputs;
+		
+		//need to add biases
+		for (int m=0;m<this.weights.length;m++) {
+			if (m == 0) {
+				current_gen = Matrix.multiply(this.weights[m], inputs);
+				current_gen = Matrix.add(current_gen, this.biases[m]);
+				//activation function
+				for (int i=0; i<current_gen.rows;i++) {
+					for (int j=0; j<current_gen.cols;j++) {
+						current_gen.data[i][j] = sigmoid(current_gen.data[i][j]);
+					}
+				}
+			}
+			else {
+				current_gen = Matrix.multiply(this.weights[m],current_gen);
+				current_gen = Matrix.add(current_gen, this.biases[m]);
+				for (int i=0; i<current_gen.rows;i++) {
+					for (int j=0; j<current_gen.cols;j++) {
+						current_gen.data[i][j] = sigmoid(current_gen.data[i][j]);
+					}
+				}
+			}
+			hiddens[m] = current_gen;
+			//hiddens[m].print();
+			//System.out.println(m);
+		}
+		//Matrix outputs = current_gen;
+		////FEEDFORWARD END ///////////
+	
+		Matrix outputs = current_gen;
+		//Convert array to matrix object
+		//Matrix outputs_ = Matrix.fromArray(outputs);
+		Matrix targets_ = Matrix.fromArray(target_array);
+		
+		//Calculate the error
+		//Error = TARGETS - OUTPUTS
+		Matrix[] errors = new Matrix[this.weights.length];
+		errors[this.weights.length-1] = Matrix.subtract(targets_, outputs); 
+		
+		
+		//Calculate the hidden errors
+		//For each hidden layer we calculate the hidden errors and update the weights
+		Matrix wh_t = null;
+		Matrix gradients = null;
+		Matrix hidden_T = null;
+		Matrix c_weight_deltas = null;
+		for (int m=this.weights.length-1; m > -1; m--) {
+			if (m == this.weights.length-1) {
+				gradients = new Matrix(outputs.rows,outputs.cols);
+				for (int i=0; i<outputs.rows;i++) {
+					for (int j=0; j<outputs.cols;j++) {
+						gradients.data[i][j] = dsigmoid(outputs.data[i][j]);
+					}
+				}
+				gradients.multiply(errors[m]);
+				gradients.multiply(this.learning_rate);
+				
+				//calculate Deltas
+				hidden_T = Matrix.transpose(hiddens[m-1]);
+				c_weight_deltas = Matrix.multiply(gradients,hidden_T);
+								
+				
+				this.weights[m] = Matrix.add(this.weights[m],c_weight_deltas);
+				this.biases[m] = Matrix.add(this.biases[m],gradients);
+				
+			}
+			
+			else if (m==0) {
+				wh_t = Matrix.transpose(this.weights[m+1]);
+				errors[m] = Matrix.multiply(wh_t,errors[m+1]);
+				
+				gradients = new Matrix(hiddens[m].rows,hiddens[m].cols);
+				for (int i=0; i<hiddens[m].rows;i++) {
+					for (int j=0; j<hiddens[m].cols;j++) {
+						gradients.data[i][j] = dsigmoid(hiddens[m].data[i][j]);
+					}
+				}
+				
+				gradients.multiply(errors[m]);
+				gradients.multiply(this.learning_rate);
+				
+				Matrix inputs_T = Matrix.transpose(inputs);
+				Matrix weight_ih_deltas = Matrix.multiply(gradients, inputs_T);
+				
+				this.weights[m] = Matrix.add(this.weights[m],weight_ih_deltas);
+				//Adjust the bias by its deltas (which is just the gradient)
+				this.biases[m] = Matrix.add(this.biases[m], gradients);
+				
+			}
+		
+			else {
+				//Find the error of the hidden layer
+				wh_t = Matrix.transpose(this.weights[m+1]);
+				errors[m] = Matrix.multiply(wh_t, errors[m+1]);
+				
+				//Calculate the gradient
+				gradients = new Matrix(hiddens[m].rows,hiddens[m].cols);
+				for (int i=0; i<hiddens[m].rows;i++) {
+					for (int j=0; j<hiddens[m].cols;j++) {
+						gradients.data[i][j] = dsigmoid(hiddens[m].data[i][j]);
+					}
+				}
+				gradients.multiply(errors[m]);
+				gradients.multiply(this.learning_rate);
+				
+				//Calculate hidden deltas
+				hidden_T = Matrix.transpose(hiddens[m-1]);
+				c_weight_deltas = Matrix.multiply(gradients,hidden_T);
+								
+				//Update weights
+				this.weights[m] = Matrix.add(this.weights[m],c_weight_deltas);
+				this.biases[m] = Matrix.add(this.biases[m],gradients);
+			}
+		}
+	}
+
+
 	
 	
 
--- src/main/java/nic/NeuralNet.java
+++ src/main/java/nic/NeuralNet.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 public class NeuralNet {
 	
 	static float randomInt(float a,float b) {
-		float rand = (float)(Math.random() * ((b - a)+1))+a ;
+		float rand = (float)(a + Math.random() * (b - a));
 		return rand;
 		
 	}
 	
 	static NeuralNetwork setup() {
-		NeuralNetwork nn = new NeuralNetwork(2,5,2);
+		NeuralNetwork nn = new NeuralNetwork(2,16,2);
+		
 		
 		//XOR dataset -> aim to replace with game data
 		float[][] x_train = {{0,0},{0,1},{1,0},{1,1}};
@@ -19,28 +20,44 @@ public class NeuralNet {
 		
 		//where [0,1] -> false and [1,0] -> is true one_hot_encoding
 		
-		//float[] input = {0,0};
-		//float[] target = {0,1};
-		//nn.train(input,target);
-		float item;
-		for (int i=0;i<10000;i++) {
+		float[] input = {0,0};
+		float[] target = {0,1};
+		nn.train(input,target);
+		//float item;
+		//for (int i=0;i<10000;i++) {
 			//System.out.println(Arrays.deepToString(x_train[j]))
-			item = randomInt(0,x_train.length-1);
-			nn.train(x_train[(int)item], y_train[(int)item]);
-		}
+		//	item = randomInt(0,x_train.length-1);
+		//	nn.train(x_train[(int)item], y_train[(int)item]);
+		//}
 		
 		return nn;
 	}
 	
 	public static void main(String[] args) {
 		
-		NeuralNetwork nn = setup();
-		float[] input = {0,0};
+		//NeuralNetwork mm = setup();
+		float[] nnShape = {2,8,2};
+		float lr = (float)0.001;
+		MultLayeredNN nn = new MultLayeredNN(nnShape, lr);
+		
+		//XOR dataset -> aim to replace with game data
+		float[][] x_train = {{0,0},{0,1},{1,0},{1,1}};
+		float[][] y_train = {{0,1},{1,0},{1,0},{0,1}};
+		
+		float item;
+		for (int i=0;i<10000;i++) {
+			//System.out.println(Arrays.deepToString(x_train[j]))
+			item = randomInt(0,x_train.length);
+			nn.train(x_train[(int)item], y_train[(int)item]);
+		}
+		
+		//nn.train(x_train[0],y_train[0]);
+		
+		float[] input = {0,1};
 		float[] guess = nn.feedforward(input);
 		System.out.println(Arrays.toString(guess));
-
 		
-
+	
 	}
 
 }
