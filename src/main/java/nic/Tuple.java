c230f849b6669526101147e96e39c454d1e803f5
JamieS1211 <jamieshepherd1211@gmail.com>
2020-01-10 21:01:14 +0000

    Added comments / formating

--- src/main/java/nic/Tuple.java
+++ src/main/java/nic/Tuple.java
@@ -2,82 +2,91 @@ package nic;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-import put.ci.cevo.games.game2048.Game2048Board;
 import put.ci.cevo.util.Pair;
 
+public class Tuple implements Serializable {
 
-
-public class Tuple implements Serializable{
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -94781376176241568L;
-	private double[] tupleLookupTable;
-	private ArrayList<Pair<Integer,Integer>> tupleCells; 
+	private final double[] tupleLookupTable;
+	private final ArrayList<Pair<Integer, Integer>> tupleCells;
 	private int tupleKey; 
-	private double current_value;
-	Tuple(double[] lookup,ArrayList<Pair<Integer,Integer>> tuple_cells){
-		this.tupleLookupTable =lookup;
-		this.tupleCells = tuple_cells;
+	private double currentValue;
+
+	/**
+	 * Constructor
+	 * @param tupleLookupTable - Lookup table
+	 * @param tupleCells - Cells of tuple
+	 */
+	Tuple(double[] tupleLookupTable, ArrayList<Pair<Integer, Integer>> tupleCells){
+		this.tupleLookupTable = tupleLookupTable;
+		this.tupleCells = tupleCells;
 
 		tupleKey = 0;
-		current_value=0;
-	} 
+		currentValue = 0;
+	}
+
 	/**
 	 * Each tuple keeps its current key used for last action evaluation.
-     * use update=true only if you are going to update the value for that action
-	 * @param afterstate
-	 * @param update - whether to keep the value for later update
-	 * @return
+     * use update = true only if you are going to update the value for that action
+	 * @param boardState - Board state after change
+	 * @param update - Keep the value for later update?
+	 * @return Value of tuple for given board
 	 */
-	public double evaluateBoard(int[][] afterstate,boolean update) {
-
+	public double evaluateBoard(int[][] boardState, boolean update) {
 		int key = 0;
 		int base15 = 1;
 		
-		for (Pair<Integer,Integer> cellPosition:tupleCells) {
-			key += base15*afterstate[cellPosition.first()][cellPosition.second()];
-			base15*=15;
+		for (Pair<Integer, Integer> cellPosition:tupleCells) {
+			key += base15 * boardState[cellPosition.first()][cellPosition.second()];
+			base15 *= 15;
 		}
+
 		if (update) {
-			this.tupleKey=key;
-			this.current_value = this.tupleLookupTable[this.tupleKey];
-			return this.current_value;
-		}
-		else {
+			this.tupleKey = key;
+			this.currentValue = this.tupleLookupTable[this.tupleKey];
+			return this.currentValue;
+		} else {
 			return this.tupleLookupTable[key];
 		}
 	}
+
 	/**
-	 * updates the stored value by incrementing it by delta
-	 * @param delta
+	 * Updates the most recent value by incrementing it by delta
+	 * @param delta - Value to increment tuple by
 	 */
 	public void update(double delta) {
-		this.tupleLookupTable[tupleKey]+=delta;
+		this.tupleLookupTable[tupleKey] += delta;
 	}
-	public double getTupleValue() {
 
-		return current_value;
-	}
 	/**
-	 * this method is not used yet but it was meant for comparing tuple shapes to possibly share the lookup table between tuples. Should be useful in tuple evaluation.
-	 * @param another_tuple
-	 * @return
+	 * Return value of tuple
+	 * @return - Current value of this tuple
 	 */
-	public boolean compareShapes(ArrayList<Pair<Integer,Integer>> another_tuple) {
-		if(another_tuple.size() !=tupleCells.size()) {
+	public double getTupleValue() {
+		return currentValue;
+	}
+
+	/**
+	 * Currently unused
+	 * Compares tuple shapes to possibly share the lookup table between tuples. Should be useful in tuple evaluation.
+	 * @param another_tuple - Tuple to compare to
+	 * @return - Are tuples same shape
+	 */
+	public boolean compareShapes(ArrayList<Pair<Integer, Integer>> another_tuple) {
+		if (another_tuple.size() != tupleCells.size()) {
 			return false;
 		}
-		Pair <Integer,Integer> difference;
-		difference= new Pair<Integer,Integer>(another_tuple.get(0).first()-tupleCells.get(0).first(),another_tuple.get(0).second()-tupleCells.get(0).second());
-		for (int i = 1;i<another_tuple.size();i++) {
-			if(!(difference.first()==another_tuple.get(i).first()-tupleCells.get(i).first() && difference.second()==another_tuple.get(i).second()-tupleCells.get(i).second())) {
+
+		Pair <Integer, Integer> difference;
+		difference = new Pair<>(another_tuple.get(0).first() - tupleCells.get(0).first(), another_tuple.get(0).second() - tupleCells.get(0).second());
+
+		for (int i = 1; i < another_tuple.size(); i++) {
+			if (difference.first() != another_tuple.get(i).first() - tupleCells.get(i).first() || difference.second() != another_tuple.get(i).second() - tupleCells.get(i).second()) {
 				return false;
 			}
 		}
+
 		return true;
 	}
 }
--- src/main/java/nic/Utils.java
+++ src/main/java/nic/Utils.java
@@ -5,102 +5,127 @@ import put.game2048.Action;
 import put.game2048.Board;
 
 public class Utils {
-	static public void printBoard(int[][] board) {
+	/**
+	 * Print the 22048 board from state
+	 * @param board - Board to print
+	 */
+	static private void printBoard(int[][] board) {
 		State2048 s = new State2048(board);
 		s.printHumanReadable();
 	}
-	static public void printBoard(Board board) { printBoard(board.get()); }
-	
-	static public int[][] simulateMove(int[][] oldBoard, Action action) {
-		/* Adapted from State2048.java/moveUP */
-		int[][] board = oldBoard.clone();
-		switch(action) {
-		case UP:
-			for (int col = 0; col < 4; col++) {
-				int firstFreeRow = 0;
-				boolean alreadyAggregated = false;
-				for (int row = 0; row < 4; row++) {
-					if (board[row][col] == 0) {
-						continue;
-					}
-					if (firstFreeRow > 0 && !alreadyAggregated && board[firstFreeRow - 1][col] == board[row][col]) {
-						board[firstFreeRow - 1][col]++;
-						board[row][col] = 0;
-						alreadyAggregated = true;
-					} else {
-						int temp = board[row][col];
-						board[row][col] = 0;
-						board[firstFreeRow++][col] = temp;
-						alreadyAggregated = false;
-					}
-				}
-			}
-			return board;
-		case DOWN:
-			for (int col = 0; col < 4; col++) {
-				int firstFreeRow = 3;
-				boolean alreadyAggregated = false;
-				for (int row = 3; row >= 0; row--) {
-					if (board[row][col] == 0) {
-						continue;
-					}
-					if (firstFreeRow < 3 && !alreadyAggregated && board[firstFreeRow + 1][col] == board[row][col]) {
-						board[firstFreeRow + 1][col]++;
-						board[row][col] = 0;
-						alreadyAggregated = true;
-					} else {
-						int temp = board[row][col];
-						board[row][col] = 0;
-						board[firstFreeRow--][col] = temp;
-						alreadyAggregated = false;
-					}
-				}
-			}
-			return board;
-		case LEFT:
-			for (int row = 0; row < 4; row++) {
-				int firstFreeCol = 0;
-				boolean alreadyAggregated = false;
-				for (int col = 0; col < 4; col++) {
-					if (board[row][col] == 0) {
-						continue;
-					}
-					if (firstFreeCol > 0 && !alreadyAggregated && board[row][firstFreeCol - 1] == board[row][col]) {
-						board[row][firstFreeCol - 1]++;
-						board[row][col] = 0;
-						alreadyAggregated = true;
-					} else {
-						int temp = board[row][col];
-						board[row][col] = 0;
-						board[row][firstFreeCol++] = temp;
-						alreadyAggregated = false;
-					}
-				}
-			}
-			return board;
-		case RIGHT:
-			for (int row = 0; row < 4; row++) {
-				int firstFreeCol = 3;
-				boolean alreadyAggregated = false;
-				for (int col = 3; col >= 0; col--) {
-					if (board[row][col] == 0) {
-						continue;
-					}
-					if (firstFreeCol < 3 && !alreadyAggregated && board[row][firstFreeCol + 1] == board[row][col]) {
-						board[row][firstFreeCol + 1]++;
-						board[row][col] = 0;
-						alreadyAggregated = true;
-					} else {
-						int temp = board[row][col];
-						board[row][col] = 0;
-						board[row][firstFreeCol--] = temp;
-						alreadyAggregated = false;
-					}
-				}
-			}
-			return board;
-		}
-		return null;
+
+	/**
+	 * Print the 2048 board from board object
+	 * @param board - Board object to print
+	 */
+	static public void printBoard(Board board) {
+		printBoard(board.get());
 	}
 
+	/**
+	 * Simulates a movement and returns the board state that would follow that move
+	 * @param board - Board state to simulate the action on
+	 * @param action - Action to simulate
+	 * @return - Board state that would result from action
+	 */
+	static public int[][] simulateMove(int[][] board, Action action) {
+		/* Adapted from State2048.java/moveUP */
+		int[][] simulatedBoard = board.clone();
+
+		switch(action) {
+			case UP:
+				for (int col = 0;  col >= 0 && col < 4; col++) {
+					int firstFreeRow = 0;
+					boolean alreadyAggregated = false;
+
+					for (int row = firstFreeRow; row >= 0 && row < 4; row++) {
+						if (simulatedBoard[row][col] == 0) {
+							continue;
+						}
+
+						if (firstFreeRow > 0 && !alreadyAggregated && simulatedBoard[firstFreeRow - 1][col] == simulatedBoard[row][col]) {
+							simulatedBoard[firstFreeRow - 1][col]++;
+							simulatedBoard[row][col] = 0;
+							alreadyAggregated = true;
+						} else {
+							simulatedBoard[row][col] = 0;
+							simulatedBoard[firstFreeRow++][col] = simulatedBoard[row][col];
+							alreadyAggregated = false;
+						}
+					}
+				}
+				return simulatedBoard;
+			case DOWN:
+				for (int col = 0; col < 4; col++) {
+					int firstFreeRow = 3;
+					boolean alreadyAggregated = false;
+
+					for (int row = 3; row >= 0; row--) {
+						if (simulatedBoard[row][col] == 0) {
+							continue;
+						}
+
+						if (firstFreeRow < 3 && !alreadyAggregated && simulatedBoard[firstFreeRow + 1][col] == simulatedBoard[row][col]) {
+							simulatedBoard[firstFreeRow + 1][col]++;
+							simulatedBoard[row][col] = 0;
+							alreadyAggregated = true;
+						} else {
+							int temp = simulatedBoard[row][col];
+							simulatedBoard[row][col] = 0;
+							simulatedBoard[firstFreeRow--][col] = temp;
+							alreadyAggregated = false;
+						}
+					}
+				}
+				return simulatedBoard;
+			case LEFT:
+				for (int row = 0; row < 4; row++) {
+					int firstFreeCol = 0;
+					boolean alreadyAggregated = false;
+
+					for (int col = 0; col < 4; col++) {
+						if (simulatedBoard[row][col] == 0) {
+							continue;
+						}
+
+						if (firstFreeCol > 0 && !alreadyAggregated && simulatedBoard[row][firstFreeCol - 1] == simulatedBoard[row][col]) {
+							simulatedBoard[row][firstFreeCol - 1]++;
+							simulatedBoard[row][col] = 0;
+							alreadyAggregated = true;
+						} else {
+							int temp = simulatedBoard[row][col];
+							simulatedBoard[row][col] = 0;
+							simulatedBoard[row][firstFreeCol++] = temp;
+							alreadyAggregated = false;
+						}
+					}
+				}
+				return simulatedBoard;
+			case RIGHT:
+				for (int row = 0; row < 4; row++) {
+					int firstFreeCol = 3;
+					boolean alreadyAggregated = false;
+
+					for (int col = 3; col >= 0; col--) {
+						if (simulatedBoard[row][col] == 0) {
+							continue;
+						}
+
+						if (firstFreeCol < 3 && !alreadyAggregated && simulatedBoard[row][firstFreeCol + 1] == simulatedBoard[row][col]) {
+							simulatedBoard[row][firstFreeCol + 1]++;
+							simulatedBoard[row][col] = 0;
+							alreadyAggregated = true;
+						} else {
+							int temp = simulatedBoard[row][col];
+							simulatedBoard[row][col] = 0;
+							simulatedBoard[row][firstFreeCol--] = temp;
+							alreadyAggregated = false;
+						}
+					}
+				}
+				return simulatedBoard;
+			default:
+				throw new IllegalStateException("Unexpected value: " + action);
+		}
+	}
 }
